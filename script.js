1bda285507d83060b8842f112080e41afbd779d4
JaegerAW <andreywjaya@gmail.com>
2024-06-13 02:33:36 +0700

    add new book function worked,  edited the have you read it yet part so now it works. delete button works on the new book added by user.

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-const harryPotter1 = new Book("Harry Potter and The Philosopher's Stone", 'J.K. Rowling', 309, 'not read yet');
-const harryPotter2 = new Book('Harry Potter and The Chamber of Secrets ', 'J.K. Rowling', 341, 'not read yet');
-const harryPotter3 = new Book("Harry Potter and The Prisoner of Azkaban", 'J.K. Rowling', 435, 'not read yet');
-const harryPotter4 = new Book("Harry Poter and The Goblet of Fire", "J.K. Rowling", 734, 'not read yet');
-const harryPotter5 = new Book("Harry Potter and The Order of the Phoenix", "J.K. Rowling", 870, 'not read yet');
+const harryPotter1 = new Book("Harry Potter and The Philosopher's Stone", 'J.K. Rowling', 309, 'No');
+const harryPotter2 = new Book('Harry Potter and The Chamber of Secrets ', 'J.K. Rowling', 341, 'No');
+const harryPotter3 = new Book("Harry Potter and The Prisoner of Azkaban", 'J.K. Rowling', 435, 'No');
+const harryPotter4 = new Book("Harry Poter and The Goblet of Fire", "J.K. Rowling", 734, 'No');
+const harryPotter5 = new Book("Harry Potter and The Order of the Phoenix", "J.K. Rowling", 870, 'No');
 
 
 
@@ -37,7 +37,7 @@ card.innerHTML = `<div class="card"  id ="book-${i}"">
 <div class="title">${myLibrary[i].title}</div>
 <div class="author">By ${myLibrary[i].author}</div>
 <div class="pages">${myLibrary[i].pages} pages</div>
-<div class="read">${myLibrary[i].read}</div>
+<div class="read">Read: ${myLibrary[i].read}</div>
 
 <button class="delete" onclick="deleteItem(${myLibrary[i].index})">Delete Book</button>
 
@@ -72,8 +72,11 @@ const newHaveRead = document.querySelector('#haveread');
 const newNotRead = document.querySelector('#notread');
 const addBookBtn = document.querySelector('#addnewbook');
 
+
+const readStatus = document.querySelector('input[type="radio"]:checked');//this returns null because when the page loads, none of the radio buttons were checked. It won't change even after you've interacted with the radio buttons because it checks only after the page loads.
+
 addBookBtn.addEventListener('click', ()=>{
-    const newBook = new Book(newTitle.value, newAuthor.value, newPages.value);
+    const newBook = new Book(newTitle.value, newAuthor.value, newPages.value, document.querySelector('input[type="radio"]:checked').value);
 
  myLibrary.push(newBook);
 createDeck();
