b4342b3eab716b8ec5c30935946b93096563c0c6
JaegerAW <andreywjaya@gmail.com>
2024-06-13 18:13:25 +0700

    everything is working, haven't tried inputting with dialog, i'll come back to this

--- script.js
+++ script.js
@@ -36,8 +36,48 @@ function createDeck() {
 
      
     const card = document.createElement('div');
-    
+    card.classList.add('card');
 
+    const title = document.createElement('div')
+    title.classList.add('title');
+    title.innerText = myLibrary[i].title;
+
+    const author = document.createElement('div');
+    author.classList.add('author');
+    author.innerText = "By " + myLibrary[i].author;
+
+    const pages = document.createElement('div');
+    pages.classList.add('pages');
+    pages.innerText = myLibrary[i].pages + " pages";
+
+    const read = document.createElement('div');
+    read.classList.add('read');
+    read.innerText = "Read: " + myLibrary[i].read;
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('delete');
+    deleteBtn.innerText = "Delete Book";
+    deleteBtn.addEventListener('click', ()=> deleteItem(myLibrary[i].index));
+
+    const changeBtn = document.createElement('button');
+    changeBtn.classList.add('changestatus');
+    changeBtn.innerText = "Change Read Status";
+    changeBtn.addEventListener('click', () => {
+        changeStatus(myLibrary[i]);
+    });
+    
+    myLibrary[i].index = i;
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(read);
+    card.appendChild(deleteBtn);
+    card.appendChild(changeBtn);
+    deck.appendChild(card);
+    
+    
+    
+/*
 myLibrary[i].index = i;//add index numbers property to every new book.
 
 card.innerHTML = `<div class="card"">
@@ -54,9 +94,10 @@ card.innerHTML = `<div class="card"">
 </div>`
 
 deck.appendChild(card);
+*/
+}
+}
 
-}
-}
 
 
     function changeStatus(book) {
@@ -66,6 +107,8 @@ deck.appendChild(card);
       else if (book.read === "No") {
         book.read = "Yes";
         }
+
+        createDeck();
     };
    
 
@@ -101,11 +144,18 @@ const addBookBtn = document.querySelector('#addnewbook');
 //const readStatus = document.querySelector('input[type="radio"]');//this returns null because when the page loads, none of the radio buttons were checked. It won't change even after you've interacted with the radio buttons because it checks only after the page loads.
 
 addBookBtn.addEventListener('click', (event)=>{
+    if (newTitle && newAuthor && newPages) {
     const newBook = new Book(newTitle.value, newAuthor.value, newPages.value, document.querySelector('input[type="radio"]:checked').value);
     
     event.preventDefault();
  myLibrary.push(newBook);
 createDeck();
+}
+    else {
+        alert("Please input all data!");
+        
+    };
+
 
 } 
 );
